e76e7fc14d20e9f1333ec98aa0a4654966e2e6c5
markmcentosh <mark_mcentosh1@baylor.edu>
2023-02-09 11:05:24 -0600

    dictionaries

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -5,7 +5,6 @@ phonebook = {'Chris':'555−1111',
              'Katie':'555−2222',
              'Joanne':'555−3333'}
 
-""""
 
 print()
 print('*****  start section 1 - print dictionary ********')
@@ -23,8 +22,6 @@ print()
 print('*****  end section 1 ********')
 print()
 
-
-"""
 
 
 print()
@@ -48,9 +45,6 @@ print()
 
 
 
-""""""
-
-
 
 print()
 print('*****  start section 3 - edit/append dictionary ********')
@@ -70,9 +64,6 @@ print('*****  end section 3 ********')
 print()
 
 
-""""""
-
-
 
 print()
 print('*****  start section 4 - delete/remove from dictionary ********')
@@ -88,10 +79,6 @@ print(phonebook)
 print()
 print('*****  end section 4 ********')
 print()
-
-
-
-""""""
 
 
 print()
@@ -116,8 +103,6 @@ print('*****  end section 5 ********')
 print()
 
 
-""""""
-
 
 print()
 print('*****  start section 6 - using get and clear ********')
@@ -136,7 +121,6 @@ print()
 print('*****  end section 6 ********')
 print()
 
-""""""
 
 print()
 print('*****  start section 7 - using pop method ********')
@@ -153,7 +137,6 @@ print()
 print('*****  end section 7 ********')
 print()
 
-""""""
 
 print()
 print('*****  start section 8 - using popitem ********')
@@ -171,7 +154,6 @@ print()
 print('*****  end section 8 ********')
 print()
 
-""""""
 
 print()
 print('*****  start section 9 - using random and converting to list ********')
--- 2_person_dict.py
+++ 2_person_dict.py
@@ -7,22 +7,25 @@ person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
 
 print(person)
+print()
 
 # print out the name of the second child
-name = 'Betty'
-
-if name in person:
-    print(person[name])
-else:
-    print(f"{name} was not found in person")
-
-print(person.list[1])
+print(person["children"][1])
+print()
 
 # print out the name of the cat
-print(person.pets)
+print(person["pets"]["cat"])
+print()
 
 # iterate through all children and print out each child
-
+for k in person["children"]:
+    print(k)
+print()
+    
 
 # print out the pets in this format:
 # type of pet: dog name of pet: Fido
+
+for k, v in person["pets"].items():
+    print(f"type of pet: {k} name of pet: {v}")
+print()
